eb6aaf21a709ad185ee35fe104a9b179f267c84c
INFLAMM AI <elecktroai@gmail.com>
2026-01-18 20:14:04 +0800

    Commit

--- inflamm-ai/Health_Tracker/flutter.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=C:\Program Files\Git\bin;C:\Program Files\Git\cmd;%PATH%
-C:\Users\Best\Downloads\inflamm-ai-ai\flutter\bin\flutter.bat %*
--- inflamm-ai/Health_Tracker/lib/smartwatch_connection/scan_screen/cubit/states.dart
+++
@@ -1,7 +0,0 @@
-abstract class ScanStates {}
-
-class ScanDevicesInitialState extends ScanStates {}
-
-class ScanCompletedSuccessState extends ScanStates {}
-
-class ScanDevicesErrorState extends ScanStates {}
